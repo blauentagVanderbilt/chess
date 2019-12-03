e16c06cba12553b1373a97d0569775ca234993bf
jslack2537 <jslack2537@gmail.com>
2019-12-03 20:36:10 +0000

    controller and tests

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,30 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
+    @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    if @game.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end 
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :white_player_id, :black_player_id)
+  end
+
 end
