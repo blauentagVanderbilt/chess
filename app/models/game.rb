3017b31ee11be160500685a663325e0c92a89307
Alisonhardison <appletini19@gmail.com>
2019-12-06 01:56:19 +0000

    is-obstructed?

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
 class Game < ApplicationRecord
 has_many :pieces
+
+  def contains_piece?(x_coord, y_coord)
+    if pieces.where("(x_position = ? AND y_position = ?)", x_position, y_position).any?
+      return true
+    else
+      return false
+    end
+  end
+
+
+
+
+
 end
--- db/migrate/20191128020254_create_pieces.rb
+++ db/migrate/20191128020254_create_pieces.rb
@@ -3,8 +3,10 @@ class CreatePieces < ActiveRecord::Migration[5.2]
     create_table :pieces do |t|
       t.integer :x_position
       t.integer :y_position
-      t.string :piece_type
-      t.integer :player_id
+      t.string :type
+      t.string :white?
+      t.string :name
+      t.integer :user_id
       t.integer :game_id
       t.datetime :created_at
       t.datetime :updated_at
