da29fff3c8bb5af9bce7f353132f6a792a47067d
Alisonhardison <appletini19@gmail.com>
2019-12-06 01:59:48 +0000

    fixed typo

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
 has_many :pieces
 
-  def contains_piece?(x_coord, y_coord)
+  def contains_piece?(x_position, y_position)
     if pieces.where("(x_position = ? AND y_position = ?)", x_position, y_position).any?
       return true
     else
