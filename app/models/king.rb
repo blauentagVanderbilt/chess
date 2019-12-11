4a7a585b3812d4f44790242ee58e19e645338f7a
Alisonhardison <appletini19@gmail.com>
2019-12-11 15:49:27 +0000

    king valid move

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,4 +1,13 @@
 class King < ChessPiece
   belongs_to :game
   belongs_to :users
+
+  def valid_move?(new_x_position, new_y_position)
+    x_distance = x_distance(new_x_position)
+    y_distance = y_distance(new_y_position)
+
+    x_distance == 1 || y_distance == 1
+  end
+
+
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,19 @@
 class Piece < ApplicationRecord
-belongs_to :game
-belongs_to :user
+  belongs_to :game
+  belongs_to :user
+
+
+
+  def x_distance(new_x_position)
+    (new_x_position - x_position).abs
+  end
+
+  def y_distance(new_y_position)
+    (new_y_position - y_position).abs
+  end
+
+
+
+
+
 end
