e1306f41d68412412612d1ab4bc0ca687307612c
Alisonhardison <appletini19@gmail.com>
2020-01-07 16:20:37 +0000

    fixed failing test

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :join, :forfeit, :index]
+  before_action :authenticate_user!, only: [:new, :create, :join, :forfeit]
   
 
   def index
